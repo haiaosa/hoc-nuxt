9ee47bc67fdf8dd95b8dab715a43eb7171beddf0
haiaosa <labatvii@gmail.com>
2019-03-28 12:23:32 +0700

    Working homeWithSidebar.vue

--- layouts/homeWithSidebar.vue
+++ layouts/homeWithSidebar.vue
@@ -1,18 +1,110 @@
 <template>
   <v-app dark>
     <v-toolbar app clipped-right>
-      <v-spacer></v-spacer>
-
+      <v-toolbar-side-icon>
+        <v-icon>home</v-icon>
+      </v-toolbar-side-icon>
       <v-toolbar-items>
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
+        <v-btn flat nuxt :to="{name: 'index'}">Trang chủ</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Trang Mô tả</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Trang đọc chuyện</v-btn>
+        <v-btn flat nuxt :to="{name: 'index'}">Trang sắp xếp</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Thể loại</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Xếp hạng</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Lịch sử</v-btn>
+        <v-btn flat nuxt :to="{name: 'index'}">Theo dõi</v-btn>
+        <v-btn flat nuxt :to="{name: 'note'}">Thảo luận</v-btn>
+        <v-btn flat nuxt :to="{name: 'index'}">Nhân sự</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon>
+        <v-icon>home</v-icon>
+      </v-toolbar-side-icon>
+      <v-toolbar-side-icon>
+        <v-badge color="red">
+          <template v-slot:badge>
+            <span>!</span>
+          </template>
+          <v-icon
+            color="grey"
+          >
+            mail
+          </v-icon>
+        </v-badge>
+      </v-toolbar-side-icon>
+      <v-toolbar-items>
+        <v-btn flat>Phong Nguyễn</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn icon v-on="on">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-tile
+              v-for="(item, index) in items"
+              :key="index"
+              @click=""
+            >
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
       </v-toolbar-items>
     </v-toolbar>
-    <v-navigation-drawer app right></v-navigation-drawer>
+    <v-navigation-drawer app right clipped>
+      <v-text-field
+        label="Tìm kiếm truyện"
+        prepend-icon="search"
+        clearable
+      ></v-text-field>
+    </v-navigation-drawer>
     <v-content>
       <v-container fluid>
-        <nuxt />
+        <v-layout row wrap>
+      <v-flex d-flex xs12 sm6 md4>
+        <v-card color="purple" dark>
+          <v-card-title primary class="title">Lorem</v-card-title>
+          <v-card-text>{{ lorem }}</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 sm6 md3>
+        <v-layout row wrap>
+          <v-flex d-flex>
+            <v-card color="indigo" dark>
+              <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex d-flex>
+            <v-layout row wrap>
+              <v-flex
+                v-for="n in 2"
+                :key="n"
+                d-flex
+                xs12
+              >
+                <v-card
+                  color="red lighten-2"
+                  dark
+                >
+                  <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex d-flex xs12 sm6 md2 child-flex>
+        <v-card color="green lighten-2" dark>
+          <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 sm6 md3>
+        <v-card color="blue lighten-2" dark>
+          <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
       </v-container>
     </v-content>
   </v-app>
@@ -21,7 +113,14 @@
 <script>
 export default {
   data: () => ({
-    drawer: null
+    drawer: null,
+    items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' }
+      ],
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
   }),
   props: {
     source: String
