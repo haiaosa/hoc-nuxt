a7628953d3eea247b7e2039104f3fbf0447fda12
haiaosa <labatvii@gmail.com>
2019-06-03 23:59:33 +0700

    playing TheMain5

--- components/SubIndex/TheMain5.vue
+++ components/SubIndex/TheMain5.vue
@@ -2,26 +2,28 @@
   <v-layout column wrap>
     <v-card color="purple lighten-2">
       <v-flex>
-        <v-subheader>Title</v-subheader>
+        <v-card nuxt :to="{ name: 'note' }">
+          <v-card-title primary-title>
+            title
+          </v-card-title>
+        </v-card>
       </v-flex>
       <v-flex>
         <v-layout row wrap>
-          <v-list>
-            <v-list-tile v-for="n in 8" :key="n" xs6>
-              <v-flex>
-                <v-list-tile-content>
-                  <v-list-tile-sub-title>subTitle</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-flex>
-            </v-list-tile>
-          </v-list>
+          <v-flex v-for="n in 8" :key="n" xs6>
+            <v-card class="purple lighten-2" flat nuxt :to="{ name: 'note' }">
+              <v-card-text>
+                noi dung day ne
+              </v-card-text>
+            </v-card>
+          </v-flex>
         </v-layout>
       </v-flex>
     </v-card>
   </v-layout>
 </template>
 <script>
-  export default {
-    props: ['items']
-  }
+export default {
+  props: ['items']
+}
 </script>
