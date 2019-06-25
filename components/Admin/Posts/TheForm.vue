31122216ee1b4d988dbdbaf829ff413d3aecf622
haiaosa <labatvii@gmail.com>
2019-06-26 01:01:55 +0700

    dang nghich upload-vuetify

--- components/Admin/Posts/TheForm.vue
+++ components/Admin/Posts/TheForm.vue
@@ -3,48 +3,25 @@
     <v-container>
       <v-layout row wrap>
         <v-flex xs12>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="name"
-              :counter="10"
-              :rules="nameRules"
-              label="Name"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              label="E-mail"
-              required
-            ></v-text-field>
-
-            <v-select
-              v-model="select"
-              :items="items"
-              :rules="[v => !!v || 'Item is required']"
-              label="Item"
-              required
-            ></v-select>
-
+          <v-form>
+            <v-text-field name="name" label="Email" id="id"></v-text-field>
+            <v-textarea
+              name="input-7-1"
+              label="Default style"
+              value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+              hint="Hint text"
+            ></v-textarea>
             <v-checkbox
+              label="label"
               v-model="checkbox"
-              :rules="[v => !!v || 'You must agree to continue!']"
-              label="Do you agree?"
-              required
+              value="value"
             ></v-checkbox>
-
-            <v-btn :disabled="!valid" color="success" @click="validate">
-              Validate
-            </v-btn>
-
-            <v-btn color="error" @click="reset">
-              Reset Form
-            </v-btn>
-
-            <v-btn color="warning" @click="resetValidation">
-              Reset Validation
-            </v-btn>
+            <v-text-field
+              name="name"
+              label="Noi dung"
+              id="id"
+              value="rieng phan nay se dung CKFinder"
+            ></v-text-field>
           </v-form>
         </v-flex>
       </v-layout>
@@ -54,34 +31,11 @@
 
 <script>
 export default {
-  data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+/.test(v) || 'E-mail must be valid'
-    ],
-    select: null,
-    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
-    checkbox: false
-  }),
-
-  methods: {
-    validate() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true
-      }
-    },
-    reset() {
-      this.$refs.form.reset()
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation()
+  data() {
+    return {
+      checkbox: true,
+      radioGroup: 1,
+      switch1: true
     }
   }
 }
