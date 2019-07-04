de869c94601ac97d1d26df17f8e24927cb9ed4a1
haiaosa <labatvii@gmail.com>
2019-07-05 02:03:50 +0700

    fixed

--- pages/admin/forgotPassword.vue
+++ pages/admin/forgotPassword.vue
@@ -1,52 +1,49 @@
 <template>
-  <v-app dark>
-    <v-content>
-      <v-container fluid grid-list-md fill-height>
-        <v-layout align-center justify-center row wrap>
-          <v-flex xs6>
-            <v-layout column wrap>
-              <v-flex>
-                <v-toolbar class="secondary">
-                  <v-toolbar-title>Take Password again?</v-toolbar-title>
-                </v-toolbar>
-              </v-flex>
-              <v-flex>
-                <p class="body-1 font-italic">
-                  Input your email address, system will send you a
-                  reset-password link.
-                </p>
-              </v-flex>
-              <v-flex>
-                <v-form>
-                  <v-layout row wrap>
-                    <v-flex xs12>
-                      <v-text-field label="Email"></v-text-field>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-btn color="secondary" large block>
-                        <v-icon left>swap_horiz</v-icon>Send</v-btn
-                      >
-                    </v-flex>
-                  </v-layout>
-                </v-form>
-              </v-flex>
-              <v-flex>
-                <v-card nuxt :to="{ name: 'admin-login' }">
-                  <v-card-text class="text-xs-center body-1">
-                    Back to Login page
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
+  <v-container fluid grid-list-md fill-height>
+    <v-layout align-center justify-center row wrap>
+      <v-flex xs6>
+        <v-layout column wrap>
+          <v-flex>
+            <v-toolbar class="secondary">
+              <v-toolbar-title>Take Password again?</v-toolbar-title>
+            </v-toolbar>
+          </v-flex>
+          <v-flex>
+            <p class="body-1 font-italic">
+              Input your email address, system will send you a reset-password
+              link.
+            </p>
+          </v-flex>
+          <v-flex>
+            <v-form>
+              <v-layout row wrap>
+                <v-flex xs12>
+                  <v-text-field label="Email"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-btn color="secondary" large block>
+                    <v-icon left>swap_horiz</v-icon>Send</v-btn
+                  >
+                </v-flex>
+              </v-layout>
+            </v-form>
+          </v-flex>
+          <v-flex>
+            <v-card nuxt :to="{ name: 'admin-login' }">
+              <v-card-text class="text-xs-center body-1">
+                Back to Login page
+              </v-card-text>
+            </v-card>
           </v-flex>
         </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  layout: 'adminNoSidebar',
   data() {
     return {
       checkbox: true
--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,75 +1,62 @@
 <template>
-  <v-app dark>
-    <v-content>
-      <v-container fluid grid-list-md fill-height>
-        <v-layout align-center justify-center row wrap>
-          <v-flex xs6>
-            <v-layout column wrap>
-              <v-flex>
-                <v-toolbar class="secondary">
-                  <v-toolbar-title>Login System</v-toolbar-title>
-                </v-toolbar>
-              </v-flex>
-              <v-flex>
-                <v-form>
-                  <v-layout row wrap>
-                    <v-flex xs12>
-                      <v-text-field
-                        name="Username"
-                        label="Username"
-                      ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-text-field
-                        name="Password"
-                        label="Password"
-                      ></v-text-field>
-                    </v-flex>
-                    <v-flex xs4>
-                      <v-checkbox
-                        v-model="checkbox"
-                        label="Remember me?"
-                      ></v-checkbox>
-                    </v-flex>
-                    <v-flex xs8>
-                      <v-layout row wrap align-content-end>
-                        <v-flex xs5 offset-xs7>
-                          <v-card
-                            class="grey darken-3"
-                            nuxt
-                            :to="{ name: 'note' }"
-                          >
-                            <v-card-text>
-                              Quên mật khẩu?
-                            </v-card-text>
-                          </v-card>
-                        </v-flex>
-                      </v-layout>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-btn
-                        color="secondary"
-                        large
-                        block
-                        nuxt
-                        :to="{ name: 'admin' }"
-                      >
-                        <v-icon left>swap_horiz</v-icon>Login</v-btn
-                      >
+  <v-container fluid grid-list-md fill-height>
+    <v-layout align-center justify-center row wrap>
+      <v-flex xs6>
+        <v-layout column wrap>
+          <v-flex>
+            <v-toolbar class="secondary">
+              <v-toolbar-title>Login System</v-toolbar-title>
+            </v-toolbar>
+          </v-flex>
+          <v-flex>
+            <v-form>
+              <v-layout row wrap>
+                <v-flex xs12>
+                  <v-text-field name="Username" label="Username"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field name="Password" label="Password"></v-text-field>
+                </v-flex>
+                <v-flex xs4>
+                  <v-checkbox
+                    v-model="checkbox"
+                    label="Remember me?"
+                  ></v-checkbox>
+                </v-flex>
+                <v-flex xs8>
+                  <v-layout row wrap align-content-end>
+                    <v-flex xs5 offset-xs7>
+                      <v-card class="grey darken-3" nuxt :to="{ name: 'note' }">
+                        <v-card-text>
+                          Quên mật khẩu?
+                        </v-card-text>
+                      </v-card>
                     </v-flex>
                   </v-layout>
-                </v-form>
-              </v-flex>
-            </v-layout>
+                </v-flex>
+                <v-flex xs12>
+                  <v-btn
+                    color="secondary"
+                    large
+                    block
+                    nuxt
+                    :to="{ name: 'admin' }"
+                  >
+                    <v-icon left>swap_horiz</v-icon>Login</v-btn
+                  >
+                </v-flex>
+              </v-layout>
+            </v-form>
           </v-flex>
         </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  layout: 'adminNoSidebar',
   data() {
     return {
       checkbox: true
