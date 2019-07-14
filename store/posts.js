e0f59e38b7423a79b5284c3d789d2b6f2151fb19
haiaosa <labatvii@gmail.com>
2019-07-15 04:15:05 +0700

    vuex posts

--- store/posts.js
+++ store/posts.js
@@ -1,31 +1,38 @@
 export const state = () => ({
-  all: []
-});
+  posts: []
+})
+
+export const mutations = {
+  ADD_POST(state, post) {
+    state.posts.push(post)
+  },
+  SET_POSTS(state, posts) {
+    state.posts = posts
+  }
+}
 
 export const actions = {
   // fetchAllPosts ({commit}) {
   //   return this.$axios.$get('posts')
-  //     .then(posts => commit('setPosts', posts))
+  //     .then(posts => commit('SET_POSTS', posts))
   // }
   // ham tren tuong duoi voi async/await nhu sau
   async fetchAllPosts({ commit }) {
-    let posts = await this.$axios.$get("posts");
+    let posts = await this.$axios.$get('posts')
 
-    commit("setPosts", posts);
+    commit('SET_POSTS', posts)
   },
 
   async fetchPost({ commit }, id) {
-    let post = await this.$axios.$get(`posts/${id}`);
+    let post = await this.$axios.$get(`posts/${id}`)
 
-    commit("setPost", post);
-  }
-};
-
-export const mutations = {
-  setPost(state, post) {
-    state.all.push(post);
+    commit('ADD_POST', post)
   },
-  setPosts(state, posts) {
-    state.all = posts;
+
+  getPosts({ commit }) {
+    // get API
+    return console.log('get Posts tu API').then(posts => {
+      commit('SET_POSTS', posts)
+    })
   }
-};
+}
